70cb1b6bb81f77d2ec894df4fdfc292eaf0a79cc
nbdkhang <nbdkhang@gmail.com>
2020-12-08 21:03:39 +0700

    login +signup + passport

---
+++ src/component/Main/main.js
@@ -0,0 +1,25 @@
+import React,{ useState } from 'react';
+import { BrowserRouter, Route, Link, Router,Switch } from "react-router-dom";
+import MenuAppBar from '../Bar/bar';
+import SignIn from '../Signin/signin';
+import SignUp from "../Signup/signup"
+import App from "../../App"
+
+export default function Main()
+{   
+    localStorage.setItem('backend',`http://localhost:3000/`);
+    
+    return(
+        <BrowserRouter>
+        <MenuAppBar/>
+        <div className="main-route-place">
+            <Switch>
+            <Route  path='/signup' component={SignUp}/>
+            <Route  path='/signin' component={SignIn}/>
+            <Route path='/app' component={App}/>
+            </Switch>
+        </div>
+        
+        </BrowserRouter>
+    );
+}
---
+++ src/component/Signup/signup.js
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const history = useHistory();
+  const url= localStorage.getItem('backend');
+
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  
+
+  const addUser = async () => {
+    console.log("add");
+    sendaddUser();
+    history.push("/signin");
+  };
+  const sendaddUser = async () => {
+    const body = {
+      username: userName,
+      password: password,
+      name: name,
+      phone:phone,
+      email:email
+    };
+    console.log(body);
+    console.log(1);
+    const res = await fetch(url + `users/signup`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                name="userName"
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="UserName"
+                autoFocus
+                onChange={(e)=>setUserName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e)=>setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="Name"
+                label="Name"
+                name="Name"
+                autoComplete="name"
+                onChange={(e)=>setName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="Phone"
+                label="Phone"
+                name="Phone"
+                autoComplete="Phone"
+                onChange={(e)=>setPhone(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={(e)=>setEmail(e.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+           
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={addUser}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
